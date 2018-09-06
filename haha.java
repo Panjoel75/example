8f14d5a6495209f402f2fd86417b2c31af1d03b9
Haha <42966301+Panjoel75@users.noreply.github.com>
2018-09-06 13:43:47 +0800

    Add files via upload

---
+++ haha.java
@@ -0,0 +1,10 @@
+public class haha
+{
+	public static void main(String args[])
+	{
+		int x = 9;
+		int y = 5;
+		System.out.println(x/(double)y);//强制转换x或者y都可以，只要有其中一个是大范围数据类型，结果就会由int型变为double型
+	
+	}
+}
